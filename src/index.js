a4f439cb75d6dfc8ee49f38806283f6ca35523ff
Dhananjay <dhananjay@pop-os.localdomain>
2023-04-09 23:37:46 +0530

    Added frontend part & wrote get api

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 const express = require("express");
+var cors = require("cors");
+const csv = require("csv-parser");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig.js");
-const sqlite3 = require("sqlite3");
+const fs = require("fs");
 
-const ApiRoutes = require("./routes/index.js");
+const sqlite3 = require("sqlite3").verbose();
+const app = express();
+app.use(cors());
 
-let db = new sqlite3.Database(":memory:", (err) => {
-  if (err) {
-    console.log("Error Occurred - " + err.message);
-  } else {
-    console.log("DataBase Connected");
-  }
-});
+// Open a database connection
+const db = new sqlite3.Database("nifty.db");
 
 const setupAndStartServer = async () => {
   //create express object
-  const app = express();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-
-  app.use("/api", ApiRoutes);
 
   app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
@@ -28,3 +24,43 @@ const setupAndStartServer = async () => {
 };
 
 setupAndStartServer();
+
+// // Create a table for the CSV data
+// db.run(
+//   "CREATE TABLE stocks (id INTEGER PRIMARY KEY, date TEXT, price REAL, instrument_name TEXT)"
+// );
+
+// // Read the CSV file and insert each row into the database
+// fs.createReadStream("historical_prices.csv")
+//   .pipe(csv())
+//   .on("data", (row) => {
+//     db.run(
+//       `INSERT INTO stocks (id, date, price, instrument_name) VALUES (?, ?, ?, ?)`,
+//       [row.id, row.date, row.price, row.instrument_name]
+//     );
+//   })
+//   .on("end", () => {
+//     console.log("CSV data successfully inserted into database");
+//     db.close();
+//   });
+
+// Define the API endpoint
+app.get("/historical-data", (req, res) => {
+  const symbol = req.query.symbol;
+  const from_date = req.query.from_date;
+  const to_date = req.query.to_date;
+
+  // Query the database for data
+  db.all(
+    `SELECT * FROM stocks WHERE instrument_name = ? AND date >= ? AND date <= ?`,
+    [symbol, from_date, to_date],
+    (err, rows) => {
+      if (err) {
+        console.error(err.message);
+        res.status(500).send("Internal Server Error");
+      } else {
+        res.send(rows);
+      }
+    }
+  );
+});
